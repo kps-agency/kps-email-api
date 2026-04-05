849a26516cc0ad75f189d7dbb8924cb50c390397
kps-agency <kps.agency.ia@gmail.com>
2026-04-05 19:04:18 +0200

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -235,9 +235,13 @@ function buildUploadedFilesHtml(files) {
 }
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', 'https://kps-agency.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,POST');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With, Content-Type, Accept, Authorization'
+  );
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
